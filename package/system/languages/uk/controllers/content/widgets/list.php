839002467aaa5695f204c3338ad553d8ca9c05f6
andriy85if <work@andriy.bokiy.name>
2017-02-07 23:04:44 +0200

    first commit

---
+++ package/system/languages/uk/controllers/content/widgets/list.php
@@ -0,0 +1,17 @@
+<?php
+
+    define('LANG_WD_CONTENT_LIST_DATASET',  'Застосувати фільтри із набору');
+    define('LANG_WD_CONTENT_LIST_RELATION', 'Застосувати фільтр по звʼязку');
+    define('LANG_WD_CONTENT_LIST_IMAGE',    'Поле зображення');
+    define('LANG_WD_CONTENT_LIST_TEASER',   'Поле короткого опису');
+    define('LANG_WD_CONTENT_LIST_DETAILS',   'Показувати автора, дату і кількість коментарів');
+    define('LANG_WD_CONTENT_LIST_STYLE',    'Стиль');
+    define('LANG_WD_CONTENT_LIST_STYLE_BASIC',      'Звичайний');
+    define('LANG_WD_CONTENT_LIST_STYLE_FEATURED',   'Журнальний');
+    define('LANG_WD_CONTENT_LIST_STYLE_TILES_BIG',  'Велика плитка');
+    define('LANG_WD_CONTENT_LIST_STYLE_TILES_SMALL','Маленька плитка');
+    define('LANG_WD_CONTENT_LIST_STYLE_COMPACT',    'Компактний');
+    define('LANG_WD_CONTENT_LIST_STYLE_CUSTOM',    'Шаблон, заданий у вкладці "Дизайн"');
+    define('LANG_WD_CONTENT_WIDGET_TYPE',    'Режим віджета');
+    define('LANG_WD_CONTENT_WIDGET_TYPE1',    'Виводити звичайний список');
+    define('LANG_WD_CONTENT_WIDGET_TYPE2',    'Виводити список схожих');
---
+++ package/system/languages/uk/controllers/moderation/moderation.php
@@ -0,0 +1,23 @@
+<?php
+
+define('LANG_MODERATION_CONTROLLER',     'Модерація');
+define('LANG_MODERATION_TITLE', 'Матеріали на модерації (%s)');
+define('LANG_MODERATION_CLEAR_TITLE', 'Матеріали до видалення (%s)');
+define('LANG_MODERATION_CLEAR_THROUGH', 'Буде видалений через %s');
+define('LANG_MODERATION_ALL_LIST', 'Весь список');
+define('LANG_MODERATION_LOG_DELETE', 'Вести лог видалень');
+define('LANG_MODERATION_LOG_RESTORE', 'Вести лог відновлень із кошика');
+define('LANG_MODERATION_CLEAR_LOG_AFTER_DELETE', 'Очищати всі логи після повного видалення допису');
+define('LANG_MODERATION_TRASH_LEFT_TIME_HINT', 'Після закінчення вказаного часу дописи будуть повністю видалені. 0 — не видаляти, не вказано - за замовчуванням.');
+define('LANG_MODERATION_ACTION_0', 'Видалення в кошик');
+define('LANG_MODERATION_ACTION_1', 'Повне видалення');
+define('LANG_MODERATION_ACTION_2', 'Відновлення із кошика');
+define('LANG_MODERATION_CLEAR_LOGS', 'Очистити список');
+define('LANG_MODERATION_CLEAR_LOGS_HINT', 'Будуть видалені всі логи поточного списку. При цьому заплановані на видалення матеріали відміняться, продовжити?');
+define('LANG_MODERATION_STATUS', 'Статус');
+define('LANG_MODERATION_SUBJECT', 'Субʼєкт');
+define('LANG_MODERATION_SUBJECT_ITEM', 'Матеріал');
+define('LANG_MODERATION_DEL_TIME', 'Час до видалення');
+define('LANG_MODERATION_DELETE_ALL', 'Всі логи видалені');
+define('LANG_MODERATION_DELETE_CUSTOM', 'Логи для "%s" успішно видалені');
+
